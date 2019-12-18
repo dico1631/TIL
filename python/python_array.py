a69aec14103f0e61e033dfdfed4fbd1126254c50
Min Jung <dicoqnfkrzl@naver.com>
2019-12-18 18:10:34 +0900

    Add python

---
+++ python/python_array.py
@@ -0,0 +1,13 @@
+array = [1,2,3,"four", True]
+print(array)
+print(array[0:3])
+print(array[4:])
+print(array[-1])
+
+dust = {'영등포구': 50, '강남구' : 40}
+print(dust)
+print(dust['영등포구'])
+
+dust2 = dict(마라탕 = 100, 소고기 = 50)
+print(dust2)
+print(dust2['마라탕'])
---
+++ python/python_array2.py
@@ -0,0 +1,11 @@
+import random
+
+dinner = ['마라탕', '부대찌개', '소고기', '갈매기살', '햄버거', '피자']
+menu = random.choice(dinner)
+print(menu)
+print(dinner[2:5])
+
+num = list(range(0,15))
+num2 = list(range(10))
+print(num)
+print(num2)
