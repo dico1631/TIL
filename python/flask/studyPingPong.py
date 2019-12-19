d2cadf83e519c62d43e58db25f45fbc6185eac87
Min Jung <dicoqnfkrzl@naver.com>
2019-12-19 22:42:33 +0900

    191219 study

--- python/flask/studyPingPong.py
+++ python/flask/studyPingPong.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/ping2")
 def ping2():
-    
     return render_template("ping2.html")
 
 @app.route("/pong2")
 def pong2():
-    return render_template("pong2.html")
+    data = request.args.get("keyword")
+    return render_template("pong2.html", data = data)
+
+@app.route("/naver2")
+def naver2():
+    return render_template("naver2.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
